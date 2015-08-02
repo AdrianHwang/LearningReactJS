23a52774e79d3708af53ac2d5d651a397769371a
AdrianHwang <boardsky@126.com>
2015-08-02 15:03:58 +0800

    update the webpack client config filse

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,7 +1,43 @@
-module.exports = {
-	entry: "./entry.js",
+var path = require('path');
+var webpack =  require('webpack');
+
+var config = {
+	context: path.join(__dirname, './public'),
+	entry: {
+		client: ['./scripts/routes']
+	},
 	output: {
-		path: __dirname,
-		filename: "bundle.js"
+		path: path.join(__dirname, '/public/build'),
+		publicPath: '/build',
+		filename: '[name].bundle.js',
+		chunkFilename: '[name].chunk.js',
+	},
+	module: {
+		loader:[
+		{
+			test: /\.dust$/,
+			loader: 'dust'
+		}]
+	},
+	resolve: {
+		extension: ['','.js', '.dust']
+	},
+	externals: {
+		backbone: 'Backbone',
+		underscore: '_',
+		jquery: 'jQuery',
+		'dust-core': 'dust'
+	},
+	plugins: [],
+
+};
+
+module.exports = config;
+webpack(config, function(err, stats){
+	if(err){
+		console.log('bunle error');
+	}else{
+		console.log('bundle complete');
 	}
-}
+
+})
